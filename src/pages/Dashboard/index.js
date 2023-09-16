2c672ab51b61588340eef688afac2a5aaa7efee4
Luke <cljuntilla91@gmail.com>
2023-09-16 17:05:36 +0800

    initial commit

---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class index extends Component {
+    render() {
+        return (
+            <div>
+                <button className='btn btn-secondary'>Dashboard</button>
+            </div>
+        );
+    }
+}
+
+export default index;
---
+++ src/pages/Register/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class index extends Component {
+    render() {
+        return (
+            <div>
+                <button className='btn btn-tertiary'>Register</button>
+            </div>
+        );
+    }
+}
+
+export default index;
